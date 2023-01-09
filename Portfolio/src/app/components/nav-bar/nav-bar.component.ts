d1e9d23a6aef358d74d24dd35731b7124ffa0f7d
brunoal7 <brunoalmiron10@hotmail.com>
2023-01-09 18:07:13 -0300

    creacion nav-bar

---
+++ Portfolio/src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  logoURL: string;
+  logged:boolean;
+
+  constructor() {
+    
+      this.logoURL="assets/LogoArgProg.png";
+      this.logged=false;
+   }
+
+  ngOnInit(): void {
+  }
+
+}
